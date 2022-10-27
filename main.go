003599d81beb1e53a26a4eafd6223242100621bc
Mateus Rezende <70781361+mrezende1@users.noreply.github.com>
2022-10-27 11:49:16 -0300

    :tada: Commit inicial

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/guilhermeonrails/api-go-gin/database"
 	"github.com/guilhermeonrails/api-go-gin/routes"
 )
-
+ 
 func main() {
 	database.ConectaComBancoDeDados()
 	routes.HandleRequest()
